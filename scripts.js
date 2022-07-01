fe800dcccc42dadd67412cfc21fe6ddbbbfce06c
Nestraz <nestrazz22@gmail.com>
2022-07-01 11:14:20 +0200

    First commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+console.log('Le script est bien changé')
+
+// capter une balsie HTML en JS 
+const burgerButton = document.querySelector('.navbar-burger')
+
+//ajouter un ecouteur d'événement 
+burgerButton.addEventListener('click , event => {
+    console.log('Clic sur le boutton')
+}) 
